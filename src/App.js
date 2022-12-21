dde4b9dbc652efe3b2170fa68438095ab54546c0
GonzaSebastian <gonzalo.stoledo@gmail.com>
2022-12-21 00:09:11 -0300

    correcion ruta logo molber

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import logomolber from './logomolber.png';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="">
-        <img src={logomolber} />
+      <header>
+        <img src="./images/logomolber.png" />
         <Navbar/>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Molber APP.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
