b82c5fdc008a7c805cf51c3a37fde10b4cbfac1f
GonzaSebastian <gonzalo.stoledo@gmail.com>
2023-02-09 20:47:50 -0300

    se agrega form en checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,17 +1,21 @@
 import { collection, query, where, documentId, getDocs, writeBatch, addDoc } from "firebase/firestore"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../../Context/CartContext"
 import { db } from "../../service/firebase/firebaseConfig"
 
 const Checkout = () => {
   const { cart, total } = useContext(CartContext)
+  const [name, setName] = useState('')
+  const [dni, setDni] = useState('')
+  const [email, setEmail] = useState('')
+  const [emailConfirm, setEmailConfirm] = useState('')
 
   const createOrder = async () => {
     const objOrder = {
       buyer: {
-        nombre: 'Gonzalo',
-        dni: "34444444",
-        email: 'gonza@gmail.com',
+        nombre: name,
+        dni: dni,
+        email: email,
       },
       items: cart,
       total
@@ -60,6 +64,12 @@ const Checkout = () => {
     <div>
       <h4>Finalizar compra</h4>
       <h2>Total: ${total}</h2>
+      <form>
+        <input type='text' placeholder='Nombre y Apellido' value={name} onChange={(e) => setName(e.target.value)} />
+        <input type='number' placeholder='DNI' value={dni} onChange={(e) => setDni(e.target.value)} />
+        <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type='email' placeholder='Confirmar email' value={emailConfirm} onChange={(e) => setEmailConfirm(e.target.value)} />
+      </form>
       <button onClick={createOrder}>Confirmar compra</button>
     </div>
   )
